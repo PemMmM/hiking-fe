be0733175d663e1bc49db4df4bccf3d0cab7d6fe
ipek <ipeksoltanov@hotmail.com>
2022-03-04 19:57:04 +0200

    fixed folder structure - navbar added

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import LandingPage from "./LandingPage";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <LandingPage/>
+    </div>
+  );
+}
+
+export default App;
