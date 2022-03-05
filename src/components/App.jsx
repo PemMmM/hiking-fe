dab1d581ae0797d83d8178e22838f1fb035dc54b
ipek <ipeksoltanov@hotmail.com>
2022-03-05 18:55:48 +0200

    LoginForm Component in process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import LandingPage from "./LandingPage";
+import LoginForm from "./LoginForm";
+
 
 function App() {
   return (
     <div>
       <Header />
       <LandingPage/>
+      <LoginForm/>
     </div>
   );
 }
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React from 'react';
+import "../styles/loginForm.css";
 
 function LoginForm() {
   return (
-    <div>LoginForm</div>
+    <div>
+      
+    <form action="">
+      <div className="image-container">
+        <img src="./images/user_icon_001.jpg" alt="avatar" className="avatar" />
+      </div>
+
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          className="username-field"
+          type="text"
+          name="username"
+          id="username"
+          minLength="2"
+          maxLength="50"
+          placeholder="Enter Username"
+          required
+        />
+        <br />
+
+        <label htmlFor="password">Password</label>
+        <input
+          className="passwor-field"
+          type="password"
+          name="password"
+          id="password"
+          minLength="8"
+          maxLength="20"
+          placeholder="Enter Password"
+          required
+        />
+        <br />
+
+        <input type="checkbox" name=""  />Remember me <br />
+
+        <input type="submit" name=""  value="Login" className="submit-btn" />
+
+        <input type="button" name=""  value="Cancel" className="cancel-btn" />
+        <br />
+
+        <a href="#" className="forgot-password">Forgot Password?</a>
+      </div>
+    </form>
+    </div>
   )
 }
 
