61a3b9861b811249d4c72f41437263f943bc760e
baris_aksay <barish.aksay@gmail.com>
2022-03-06 19:23:02 +0200

    routes completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import "../styles/header.css";
 
 function Header() {
   return (
-      <div className="wrap">
-        
-        <header className="header">
-          <h1 className="logo-title">
-            <a href="#" className="logo">
-              <img className="header-logo" src="./images/logo.svg" alt="" />
-            </a>
-          </h1>
-        </header>
-        <nav className="nav">
-              <ul className="menu-list">
-                <li className="menu-item">
-                  <NavLink className="menu-link" to="./loginForm">Log In</NavLink>                
-                </li>
-                <li className="menu-item">                
-                  <NavLink className="menu-link" to="./signUpForm">Sign Up</NavLink>                          
-                </li>
-                <li className="menu-item">
-                  <a href="#Paths" className="menu-link">
-                    Paths
-                  </a>
-                </li>
-                <li className="menu-item">
-                  <a href="#Profile" className="menu-link">
-                    Profile
-                  </a>
-                </li>
-              </ul>
-        </nav>
-      </div>
-  )
+    <div className="wrap">
+      <header className="header">
+        <h1 className="logo-title">
+          <NavLink className="menu-link" to="./">
+            <img className="header-logo" src="./images/logo.svg" alt="" />
+          </NavLink>
+        </h1>
+      </header>
+      <nav className="nav">
+        <ul className="menu-list">
+          <li className="menu-item">
+            <NavLink className="menu-link" to="./loginForm">
+              Log In
+            </NavLink>
+          </li>
+          <li className="menu-item">
+            <NavLink className="menu-link" to="./signUpForm">
+              Sign Up
+            </NavLink>
+          </li>
+          <li className="menu-item">
+            <NavLink className="menu-link" to="./paths">
+              Paths
+            </NavLink>
+          </li>
+          <li className="menu-item">
+            <NavLink className="menu-link" to="./profile">
+              Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,7 @@ export { default as LoginForm } from "./LoginForm";
 export { default as SignUpForm } from "./SignUpForm";
 export { default as Footer } from "./Footer";
 export { default as LandingPage } from "./LandingPage";
+export { default as Paths } from "./Paths";
+export { default as UserProfile } from "./UserProfile";
+
+
